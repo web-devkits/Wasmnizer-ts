ff0d6154b7fd56e8243e9fa6a8d619ec6db8e4b6
Shanks0224 <130947810+Shanks0224@users.noreply.github.com>
2023-10-16 22:55:37 +0800

    fix: function type comparison adds 'restParamIdx' comparison item (#25)
    Signed-off-by: ganjing <ganjing@xiaomi.com>

--- src/semantics/semantics_nodes.ts
+++ src/semantics/semantics_nodes.ts
@@ -866,7 +866,7 @@ export class ModuleNode extends SemanticsNode {
                         const f = t as FunctionType;
                         if (!f.returnType.equals(retType)) continue;
                         if (params.length != f.argumentsType.length) continue;
-                        // TODO has reset paramters
+                        if (f.restParamIdx != ts_func.restParamIdx) continue;
                         let i = 0;
                         for (i = 0; i < params.length; i++) {
                             if (!params[i].equals(f.argumentsType[i])) break;
--- tests/samples/class_direct_call.ts
+++ tests/samples/class_direct_call.ts
@@ -32,3 +32,40 @@ export function foo() {
     }
     a.test();
 }
+
+class Foo {
+    method_foo1(arr: number[]): number {
+        return 1;
+    }
+
+    method_foo2(a: number, ...b: number[]) {
+        const c = a + b[0] + b[1];
+        return c;
+    }
+
+    method_foo3(a: number, ...b: number[]) {
+        return a;
+    }
+
+    method_foo4(...num: number[]) {
+        let res = 0;
+        for (let i = 0; i < num.length; i++) {
+            res += num[i];
+        }
+        return res;
+    }
+}
+
+function func_foo(arr: number[]): number {
+    return 2;
+}
+
+export function test() {
+    const a = new Foo();
+    const arr: number[] = [1, 3, 5, 7];
+    console.log(a.method_foo1(arr));
+    console.log(a.method_foo2(1, 2, 3, 4) + a.method_foo3(5));
+    console.log(a.method_foo4(1, 2));
+    console.log(a.method_foo4());
+    console.log(func_foo(arr));
+}
