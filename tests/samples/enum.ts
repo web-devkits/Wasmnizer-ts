452ec52358e342b2ee86216bbd1fddafa0118289
WenLY1 <130950131+WenLY1@users.noreply.github.com>
2023-11-22 10:11:34 +0800

    correct the initial value of enum members (#69)
    Signed-off-by: wenlingyun1 <wenlingyun1@xiaomi.com>

--- tests/samples/enum.ts
+++ tests/samples/enum.ts
@@ -3,13 +3,6 @@
  * SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
  */
 
-enum Color2 {
-    RED,
-    GREEN,
-    BLUE,
-    WHITE = "white"
-}
-
 enum Color1 {
     RED = 'red',
     GREEN = 'green',
@@ -17,17 +10,36 @@ enum Color1 {
     WHITE = 0,
 }
 
-export function digitEnum() {
+enum Color2 {
+    RED,
+    GREEN,
+    BLUE,
+    WHITE = "white"
+}
+
+enum Color3 {
+    RED = 1,
+    GREEN,
+    BLUE,
+    WHITE = 0,
+    PINK,
+}
+
+export function assignDigitMember() {
     let red: Color2 = Color2.RED;
     if (red === Color2.RED) {
         console.log(red);
     }
 }
 
-export function stringEnum() {
+export function assignStringMember() {
     let red: Color1 = Color1.RED;
     if (red === Color1.RED) {
         console.log(red);
     }
 }
 
+export function enumInitialize() {
+    console.log(Color3.BLUE);
+    console.log(Color3.PINK);
+}
