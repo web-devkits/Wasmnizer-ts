ee16a113fa24e492333cdd0ba7b8722bfcbc9bf1
Su Yihan <yihan.su@intel.com>
2023-11-24 16:25:26 +0800

    add license header
    Signed-off-by: Su Yihan <yihan.su@intel.com>

--- tests/benchmark/fibonacci.ts
+++ tests/benchmark/fibonacci.ts
@@ -1,3 +1,8 @@
+/*
+ * Copyright (C) 2023 Intel Corporation.  All rights reserved.
+ * SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+ */
+
 function fibonacci(num: number): number {
     if (num < 2) {
         return num;
