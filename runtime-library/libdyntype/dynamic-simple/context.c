67e5c4d00e8c3e071cf788193b258c531d240ca5
Xu Jun <jun1.xu@intel.com>
2023-12-12 14:50:39 +0800

    implement simple libdyntype
    Signed-off-by: Xu Jun <jun1.xu@intel.com>

---
+++ runtime-library/libdyntype/dynamic-simple/context.c
@@ -0,0 +1,32 @@
+/*
+ * Copyright (C) 2023 Intel Corporation.  All rights reserved.
+ * SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+ */
+
+#include "pure_dynamic.h"
+
+static dyn_ctx_t g_dynamic_context = NULL;
+
+/******************* Initialization and destroy *****************/
+
+dyn_ctx_t
+dynamic_context_init()
+{
+    return g_dynamic_context;
+}
+
+dyn_ctx_t
+dynamic_context_init_with_opt(dyn_options_t *options)
+{
+    return g_dynamic_context;
+}
+
+void
+dynamic_context_destroy(dyn_ctx_t ctx)
+{}
+
+dyn_ctx_t
+dynamic_get_context()
+{
+    return g_dynamic_context;
+}
--- runtime-library/libdyntype/libdyntype.cmake
+++ runtime-library/libdyntype/libdyntype.cmake
@@ -7,10 +7,24 @@ set (LIBDYNTYPE_DIR ${CMAKE_CURRENT_LIST_DIR})
 
 include_directories (${LIBDYNTYPE_DIR})
 
+if (NOT USE_SIMPLE_LIBDYNTYPE EQUAL 1)
+    message("     * Use libdyntype implemented based on quickjs")
+    include_directories(${LIBDYNTYPE_DIR}/dynamic-qjs)
+    file (GLOB dynamic_impl_src
+        ${LIBDYNTYPE_DIR}/dynamic-qjs/*.c
+    )
+else()
+    message("     * Use simple libdyntype implementation")
+    include_directories(${LIBDYNTYPE_DIR}/dynamic-simple)
+    file (GLOB dynamic_impl_src
+        ${LIBDYNTYPE_DIR}/dynamic-simple/*.c
+    )
+endif()
+
 file (GLOB source_all
     ${LIBDYNTYPE_DIR}/*.c
     ${LIBDYNTYPE_DIR}/extref/*.c
-    ${LIBDYNTYPE_DIR}/dynamic/*.c
+    ${dynamic_impl_src}
 )
 
 set (LIBDYNTYPE_SRC ${source_all})
