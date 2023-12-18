a8c463af71ebee9da7478668bac926bc7f9f08eb
ganjing <ganjing@xiaomi.com>
2023-12-18 12:29:49 +0800

    improvement: optimize the logic of determining whether a file is a standard library
    Signed-off-by: ganjing <ganjing@xiaomi.com>

---
+++ tests/samples/lib.ts
@@ -0,0 +1,8 @@
+/*
+ * Copyright (C) 2023 Xiaomi Corporation.  All rights reserved.
+ * SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+ */
+
+export function test() {
+    console.log('hello')
+}
