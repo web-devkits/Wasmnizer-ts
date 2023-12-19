bf3ff606f356a91385a9aa9b74ead4922aaf5141
Xu Jun <jun1.xu@intel.com>
2023-12-20 02:51:16 +0800

    remove unused import

--- scripts/copy_lib_files.js
+++ scripts/copy_lib_files.js
@@ -6,7 +6,6 @@
 import fs from 'fs';
 import path from 'path';
 import { fileURLToPath } from 'url';
-import copyfiles from 'copyfiles';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
