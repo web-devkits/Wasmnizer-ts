67e6fb4028c49c19a72ab311dc43d98c1ddba53b
zhenweijin <zhenwei.jin@intel.com>
2023-12-27 14:24:43 +0800

    benchmark: add some micro benchmarks
    Signed-off-by: zhenweijin <zhenwei.jin@intel.com>

---
+++ tests/benchmark/class_access.ts
@@ -0,0 +1,31 @@
+/*
+ * Copyright (C) 2023 Intel Corporation.  All rights reserved.
+ * SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+ */
+
+class Foo {
+    x: number;
+    bar() {
+        return this.x;
+    }
+    constructor(x: number) {
+        this.x = x;
+    }
+}
+
+const size = 1e7;
+const expect = 99999970000002;
+let res = 0;
+const f = new Foo(0);
+
+export function main() {
+    for (let i = 0; i < size; i++) {
+        res += f.x;
+        res += f.bar();
+        f.x = i;
+    }
+    if (res !== expect) {
+        console.log('Validate result error in class access');
+    }
+    return res;
+}
---
+++ tests/benchmark/interface_access_method_slowpath.js
@@ -0,0 +1,38 @@
+/*
+ * Copyright (C) 2023 Intel Corporation.  All rights reserved.
+ * SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+ */
+
+"use strict";
+
+var Bar = /** @class */ (function () {
+    function Bar(x) {
+        this.y = 'str';
+        this.z = false;
+        this.x = x;
+    }
+    Bar.prototype.baz = function () {
+        return this.y;
+    };
+    Bar.prototype.bar = function (val) {
+        this.x = val;
+        return this.x;
+    };
+    return Bar;
+}());
+var size = 1e6;
+var expect = 499999500000;
+var f = new Bar(0);
+var res = 0;
+function main() {
+    for (var i = 0; i < size; i++) {
+        res += f.bar(i);
+    }
+    if (res !== expect) {
+        console.log(
+            'Validate result error in interface access method (slow path)',
+        );
+    }
+    return res;
+}
+console.log(main());
